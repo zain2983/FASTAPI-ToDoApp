7703da1a94c91c3594fbcf9d5b9837531a0c8462
zain2983 <zainijaz2983@gmail.com>
2025-01-10 17:19:49 +0500

    project initialization

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <TaskList />
+    </div>
+  )
+}
+
+export default App
